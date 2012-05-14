3428c3e06700471599c6ff9070bcaf679bf9467f
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-05-14 15:12:01 +0200

    ZarzadcaZadan added

---
+++ src/ZarzadcaZadan.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include "EvolException.hpp"
+
+typedef int time_type;
+
+class ZarzadcaZadan
+{
+    time_type **czasy;
+    int iloscMaszyn, iloscZadan;
+    
+    private:
+    ZarzadcaZadan();
+    ZarzadcaZadan( std::string fileName ) throw ( evol::CannotOpenFileException );
+    ZarzadcaZadan(ZarzadcaZadan const&);
+    void operator=(ZarzadcaZadan const&);
+    public:
+    ~ZarzadcaZadan();
+    static ZarzadcaZadan& getInstance( const std::string& fileName = "zadania.dat");
+    int getIloscMaszyn();
+    int getIloscZadan();
+    time_type czasObrobki( int idMaszyny, int idZadania );
+};
+
