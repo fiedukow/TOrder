760953815547d12a1b91000df2c3bd9f3a1d9756
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-05-30 20:02:45 +0200

    HTML Table generattion removed from main file

--- src/TOrder.cpp
+++ src/TOrder.cpp
@@ -41,17 +41,5 @@ int main( int argc, char* argv[] )
     wynik->print();
     std::cout << "Otrzymano w pokoleniu #" << podgladP->getGenerationNumber() << std::endl;
     std::cout << "Czas szukania " << podgladP->getTimeTaken() << "s." << std::endl;
-    std::cerr  << "<tr><td>";
-    std::cerr  << ZarzadcaZadan::getInstance().getIloscMaszyn() << "</td><td>";
-    std::cerr  << ZarzadcaZadan::getInstance().getIloscZadan()  << "</td><td>";
-    std::cerr  << ((CzasWykonania*)&*populacja.getCurrentBestFF())->getTimeTotal() << "</td><td>";
-    std::cerr  << argv[2] << "</td><td>"; //reference result
-    std::cerr  << argv[3] << "</td><td>"; //population size
-    std::cerr  << argv[4] << "</td><td>"; //mutateFactor
-    std::cerr  << argv[5] << "</td><td>"; //crossFactor
-    std::cerr  << podgladP->getLastBetter() << "</td><td>";
-    std::cerr  << podgladP->getGenerationNumber() << "</td><td>";
-    std::cerr  << podgladP->getTimeTaken() << "s.</td><td>";
-    std::cerr  << "<a href=\"../" << argv[1] << "\">" << argv[1] << "</a></td></tr>";
     return 0;
 }
